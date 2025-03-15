4dade77d3e5f3057bb0a1abdab96b0080c563977
KristinaFinck <kristina.finck83@gmail.com>
2025-03-15 23:55:29 +0200

    feat: moved selectAppStatus to a separate file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,9 +4,10 @@ import { GlobalError } from './GlobalError/GlobalError.tsx'
 import {LinearLoader} from "../common/components/Loader/LinearLoader.tsx";
 import {useSelector} from "react-redux";
 import {AppRootState, useAppSelector} from "./store.ts";
+import {selectAppStatus} from "./app-selectors.ts";
 
 export const App = () => {
-const status = useAppSelector((state) => state.app.status)
+const status = useAppSelector(selectAppStatus)
   return (
     <div>
         {status === 'loading' && < LinearLoader/>}
